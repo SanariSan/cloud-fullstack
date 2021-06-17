1240a5ed8ae54b8ed1c33c679348ba0e4642e002
SanariSan <SanariSan@users.noreply.github.com>
2021-06-17 18:59:34 +0300

    'build'

--- replace.js
+++ replace.js
@@ -3,20 +3,37 @@ import path from "path";
 import util from "util";
 
 const renameAsync = util.promisify(fs.rename);
+const rmAsync = util.promisify(fs.rm);
+const statAsync = util.promisify(fs.stat);
+const mkdirAsync = util.promisify(fs.mkdir);
 
 //---
-const reactBuildPath = path.join("./", "./", "cloud-front-build");
+const reactBuildPath = path.join("./", "cloud-front", "build");
 const targetBuildPath = path.join("./", "cloud-front-build");
 //---
 
 async function deleteEntity() {
-	await rmAsync(targetBuildPath, {
-		recursive: true,
-	});
+	const statObj = await statAsync(targetBuildPath).catch((err) => void 0);
+
+	if (statObj) {
+		await rmAsync(targetBuildPath, {
+			recursive: true,
+		});
+	}
 }
 
 async function copyEntity() {
+	const statObjBuild = await statAsync(reactBuildPath).catch((err) => void 0);
+	const statObjTarget = await statAsync(targetBuildPath).catch((err) => void 0);
+
+	if (!statObjBuild) throw new Error("no build");
+	if (!statObjTarget) await createFolder();
+
 	await renameAsync(reactBuildPath, targetBuildPath);
+}
+
+async function createFolder() {
+	await mkdirAsync(targetBuildPath);
 }
 
 deleteEntity();
